caf01d8abb8860fefa0eaba149c925c484539292
Katya <capkat@bk.ru>
2024-03-12 12:07:07 +0300

    routine

--- hw_boolean_search.py
+++ hw_boolean_search.py
@@ -4,18 +4,75 @@
 import argparse
 import codecs
 import sys
+import string
+import re
 
 
 class Index:
     def __init__(self, index_file):
-        # TODO: build index
-        pass
+        self._file = codecs.open(index_file, mode='r', encoding='utf-8')
+        self._index_dict = dict()
+        self._add_docs()
+        self._file.close()
+
+    def _add_one_doc(self, index, words):
+        for word in words:
+            lower_word = word.lower().translate(str.maketrans('', '', string.punctuation))
+            if not lower_word:
+                continue
+            if lower_word not in self._index_dict:
+                self._index_dict[lower_word] = set()
+            self._index_dict[lower_word].add(index)
+
+    def _add_docs(self):
+        for line in self._file.readlines():
+            line = line.strip()
+            index, words = line.split()[0], line.split()[1:]
+            self._add_one_doc(index, words)
+
+    def get_ids_by_word(self, word):
+        return self._index_dict[word] if word in self._index_dict else set()
 
 
 class QueryTree:
     def __init__(self, qid, query):
-        # TODO: parse query and create query tree
-        pass
+        self.query_id = qid
+        self.query = ' '.join(query.lower().split())
+        self.i = 0
+        self.c = None
+        self.words = [word for word in re.split(r'\(|\)|\|| ', self.query.strip()) if word]
+        self.query_tree = []
+    #     каждая хуйня будет словарем, ключ - это операция, элементы - это наши слова из запроса
+
+    def get(self):
+        if self.i < len(self.words):
+            self.c = self.words[self.i]
+            self.i += 1
+        else:
+            self.c = None
+
+    def parse_query(self):
+        self.get()
+        result = self._or()
+
+    def _or(self):
+        operand1 = self._and()
+        if self.c == '|':
+            self.get()
+            operand2 = self._and()
+            return {
+                'op': 'or',
+                'operand1': operand1,
+                'operand2': operand2
+            }
+        else:
+            return operand1
+
+    def _and(self):
+        operand1 = self._token()
+        if self.c == ' ':
+            operand2 = self._token()
+
 
     def search(self, index):
         # TODO: lookup query terms in the index and implement boolean search logic
@@ -35,10 +92,10 @@ class SearchResults:
 def main():
     # Command line arguments.
     parser = argparse.ArgumentParser(description='Homework: Boolean Search')
-    parser.add_argument('--queries_file', required = True, help='queries.numerate.txt')
-    parser.add_argument('--objects_file', required = True, help='objects.numerate.txt')
-    parser.add_argument('--docs_file', required = True, help='docs.tsv')
-    parser.add_argument('--submission_file', required = True, help='output file with relevances')
+    parser.add_argument('--queries_file', required=True, help='queries.numerate.txt')
+    parser.add_argument('--objects_file', required=True, help='objects.numerate.txt')
+    parser.add_argument('--docs_file', required=True, help='docs.txt')
+    parser.add_argument('--submission_file', required=True, help='output file with relevances')
     args = parser.parse_args()
 
     # Build index.
