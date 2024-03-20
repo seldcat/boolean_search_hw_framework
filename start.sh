9eb29713f034e742619817cec571e210088e3c8f
Katya <capkat@bk.ru>
2024-03-20 23:39:29 +0300

    add dockerfile

--- start.sh
+++ start.sh
@@ -1,5 +1,2 @@
-docker run -it --rm --name boolean-search -v "$PWD":/work_dir -w /work_dir python:3.12-alpine python hw_boolean_search.py \
-    --queries_file /work_dir/data/queries.numerate.txt \
-    --objects_file  /work_dir/data/objects.numerate.txt\
-    --docs_file /work_dir/data/docs.txt \
-    --submission_file /work_dir/output.csv
+docker build -t boolean-search .
+docker run -it boolean-search
